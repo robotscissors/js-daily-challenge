f7f1a3caa94bf70cebc54b440030ac273fbbd758
Christopher Bazin <cjbazin@gmail.com>
2022-01-12 15:20:54 -0800

    Homework assignment 1-11-22

---
+++ js/main.js
@@ -0,0 +1,7 @@
+// This is a single line comment
+
+/* This is a multiline comment
+	 I am sure it will be a good comment */
+
+   console.log("My name is: Christopher Bazin");
+   console.log("I love Pizza and potato chips (just not a the same time!)");
